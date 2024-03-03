1441a6bb033bc51b253ae522b6dc9611ed2ea393
mkliac <90816129+mkliac@users.noreply.github.com>
2024-03-03 16:26:32 +0800

    add radio button next to progress bar

--- src/components/PollItemsButton.tsx
+++ src/components/PollItemsButton.tsx
@@ -1,7 +1,15 @@
-import { Box, Button, LinearProgress, Stack, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  LinearProgress,
+  Radio,
+  Stack,
+  Typography,
+} from "@mui/material";
 import { Poll, PollItem } from "../models/PollModels";
 import { Dispatch, SetStateAction, useEffect, useState } from "react";
 import PollService from "../services/PollService";
+import FlexBetween from "./FlexBwtween";
 
 const PollItemsButton = ({
   poll,
@@ -45,26 +53,30 @@ const PollItemsButton = ({
             },
             border: "0.13rem solid",
             borderColor: checkItemId === item.id ? "black" : "#dcdcdc",
+            padding: "0.4rem",
           }}
           onClick={() => onVote(item)}
         >
-          <Box sx={{ width: "100%" }}>
-            <Box sx={{ display: "flex", justifyContent: "space-between" }}>
-              <Typography>{item.text}</Typography>
-              <Typography>{`${Math.round(
-                calItemContribution(item)
-              )}%`}</Typography>
-            </Box>
-            <Box sx={{ display: "flex", alignItems: "center" }}>
-              <Box sx={{ width: "100%" }}>
-                <LinearProgress
-                  sx={{ height: "1rem" }}
-                  variant="determinate"
-                  value={calItemContribution(item)}
-                />
+          <FlexBetween width="100%" gap="0.4rem">
+            <Radio checked={checkItemId === item.id} sx={{ padding: "0" }} />
+            <Box sx={{ width: "100%" }}>
+              <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+                <Typography>{item.text}</Typography>
+                <Typography>{`${Math.round(
+                  calItemContribution(item)
+                )}%`}</Typography>
+              </Box>
+              <Box sx={{ display: "flex", alignItems: "center" }}>
+                <Box sx={{ width: "100%" }}>
+                  <LinearProgress
+                    sx={{ height: "1rem", borderRadius: "0.5rem" }}
+                    variant="determinate"
+                    value={calItemContribution(item)}
+                  />
+                </Box>
               </Box>
             </Box>
-          </Box>
+          </FlexBetween>
         </Button>
       ))}
     </Stack>
