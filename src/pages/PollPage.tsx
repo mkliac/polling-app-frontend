8ee756cfdc652c768d1ecb8eb3dbd1ba78f28ea2
mkliac <90816129+mkliac@users.noreply.github.com>
2024-03-17 18:56:22 +0800

    reuse pollwidget for polls/{id}

--- src/pages/PollPage.tsx
+++ src/pages/PollPage.tsx
@@ -1,139 +1,41 @@
-import { Add, DeleteForever, EventBusy, Remove } from "@mui/icons-material";
 import {
-  Box,
-  Card,
-  CardContent,
-  CardHeader,
-  Divider,
-  IconButton,
-  Stack,
-  Tooltip,
-  Typography,
+  Box
 } from "@mui/material";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import LoadingOverlay from "../components/LoadingOverlay";
-import PollItemsButton from "../components/PollItemsButton";
-import { Poll, PollItem } from "../models/PollModels";
-import { useAppDispatch, useAppSelector } from "../redux/hook";
-import { selectUser } from "../redux/reducers/AuthSlice";
-import { selectPoll } from "../redux/reducers/PollSlice";
-import { closePoll, deletePoll, getPoll, vote } from "../services/PollService";
+import PollWidget from "../components/PollWidget";
+import { Poll } from "../models/PollModels";
+import { useAppDispatch } from "../redux/hook";
+import { getPoll } from "../services/PollService";
 
 const PollForm = () => {
   const { id } = useParams();
   const dispatch = useAppDispatch();
   const [poll, setPoll] = useState<Poll>(undefined);
-  const [isLoading, setIsLoading] = useState(false);
-  const user = useAppSelector(selectUser);
-  const [checkItemId, setCheckItemId] = useState<string>("");
 
   useEffect(() => {
-    setIsLoading(true);
-    dispatch(getPoll(id)).unwrap().then((res) => {
-      setPoll(res);
-    }).catch((err) => {
-      console.log(err);
-    }).finally(() => {
-      setIsLoading(false);
-    });
+    dispatch(getPoll(id))
+      .unwrap()
+      .then((res) => {
+        setPoll(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }, []);
-
-  const onVote = (item: PollItem) => {
-    dispatch(vote({ id: poll.id, itemId: item.id })).then((res) => {
-      if (res.meta.requestStatus === "fulfilled") setCheckItemId(item.id);
-    });
-  };
-
-  const onDeletePoll = () => {
-    dispatch(deletePoll(id));
-  };
-
-  const onClosePoll = () => {
-    dispatch(closePoll(id));
-  };
 
   return (
     <Box
       sx={{
-        minHeight: "100vh",
+        height: "100vh",
+        display: "flex",
         alignItems: "center",
         justifyContent: "center",
-        display: "flex",
       }}
     >
-      <LoadingOverlay isLoading={isLoading} />
-      {poll === undefined ? (
-        <div>Poll with id {id} not exist </div>
-      ) : (
-        <Card sx={{ width: "550px", padding: "5px 5px", margin: "0 auto" }}>
-          {user.username === poll.createdBy.username && (
-            <CardHeader
-              action={
-                <Stack direction="row">
-                  <Tooltip title="Add Options">
-                    <IconButton>
-                      <Add fontSize="small" />
-                    </IconButton>
-                  </Tooltip>
-                  <Tooltip title="Delete Options">
-                    <IconButton>
-                      <Remove fontSize="small" />
-                    </IconButton>
-                  </Tooltip>
-                  <Tooltip title="Close Poll">
-                    <IconButton onClick={() => onClosePoll()}>
-                      <EventBusy fontSize="small" />
-                    </IconButton>
-                  </Tooltip>
-                  <Tooltip title="Delete Poll">
-                    <IconButton onClick={() => onDeletePoll()}>
-                      <DeleteForever fontSize="small" />
-                    </IconButton>
-                  </Tooltip>
-                </Stack>
-              }
-            />
-          )}
-          <CardContent
-            sx={{
-              maxWidth: "550px",
-              maxHeight: "80vh",
-              overflow: "auto",
-              scrollbarGutter: "stable",
-            }}
-          >
-            <Box sx={{ marginBottom: "12px" }}>
-              {poll.closedDate && (
-                <Typography textAlign="right">
-                  Expiry Date:{" "}
-                  {new Date(poll.closedDate).toLocaleDateString("en-GB")}
-                </Typography>
-              )}
-              <Typography
-                textAlign="left"
-                variant="h3"
-                component="div"
-                sx={{ wordWrap: "break-word" }}
-              >
-                {poll.title}
-              </Typography>
-              <Typography textAlign="right">
-                {poll.createdBy?.username}
-              </Typography>
-              <Divider />
-              <Typography textAlign="left" variant="body1">
-                {poll.description}
-              </Typography>
-            </Box>
-            <PollItemsButton
-              poll={poll}
-              onVote={onVote}
-              checkItemId={checkItemId}
-            />
-          </CardContent>
-        </Card>
-      )}
+      <Box sx={{ maxWidth: "32rem" }}>
+        {poll && <PollWidget initPoll={poll} />}
+      </Box>
     </Box>
   );
 };
